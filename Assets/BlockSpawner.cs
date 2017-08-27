3547889f19ae5a3d65dc5df1e8929ca3fb8e8731
makevoid <makevoid@gmail.com>
2017-08-27 22:18:24 +0100

    initial commit
    gitignores and block spawner / websocket  class

---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,67 @@
+﻿using SimpleJSON;
+using System.Collections;
+using System;
+using UnityEngine;
+using WebSocketCSharp;
+
+public class YourScript : MonoBehaviour
+{
+  private string wsUrl = "ws://localhost:8080/";
+
+  // please run the Websocket server before pressing Run in unity - (also TODO: load the yaml config in C#)
+
+  private WebSocket ws;
+
+  // -----------------
+
+  public GameObject block;
+  public GameObject transaction;
+
+  public GameObject txPrefab;
+
+  private Rigidbody rb;
+  private int count;
+
+  private string blockUrl = "https://blockchain.info/latestblock";
+
+  // -----------------
+
+  void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    count = 0;
+
+    var ws = new WebSocket(wsUrl);
+
+    ws.OnError += (sender, e) =>
+      Console.WriteLine("WS Error");
+    // TODO: log error
+
+    ws.OnOpen += (sender, e) =>
+      Console.WriteLine("WS Connection open");
+
+    ws.Connect();
+
+    ws.OnMessage += Ws_OnMessage;
+  }
+
+  private void Ws_OnMessage(object sender, MessageEventArgs evt)
+  {
+    print("WS Message");
+
+    string text = evt.Data;
+
+    var blockData = JSON.Parse(text);
+
+    string value = blockData["op"].Value;
+    print(value); // ths should print "utx" every time you get an unconfirmed transaction
+
+    string tx = blockData["x"];
+    print(tx["hash"].Value);   // hash
+    print(tx["size"].Value);   // block size
+    //print(tx["inputs"][0][...]);  // transaction input(s)
+    //print(tx["outputs"][0][...]); // transaction output(s)
+    // look at log/ws.example.log for all the parameters
+  }
+
+}
